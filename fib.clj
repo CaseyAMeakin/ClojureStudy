d8ed720f1527ecb0b323ba27c9f78a4b9f81c55b
Casey Meakin <casey.meakin@gmail.com>
2016-01-27 19:06:10 -0800

    changed parenthesis formatting

--- fib.clj
+++ fib.clj
@@ -3,15 +3,9 @@
 ;
 
 (defn fib
-  ([n]
-   (if (== n 0) 0 (if (> n 0) (fib (- n 1) [1,1]) nil))
-   )
-  ([n,fib_]
-   (if (== n 0) (nth fib_ 0)
-       (if (> n 0) (fib (- n 1) [(nth fib_ 1), (apply + fib_)]) nil )
-       )
-   )
-)
+  ([n] (if (== n 0) 0 (if (> n 0) (fib (- n 1) [1,1]) nil)) )
+  ([n,fib_] (if (== n 0) (nth fib_ 0)
+                (if (> n 0) (fib (- n 1) [(nth fib_ 1), (apply + fib_)]) nil))))
 
 (doseq [c (range -1 20)] (println (str c) (fib c)))
 
